0554279f4b9271fe96414d6b691e7dc5d7083a02
Anilmar-Rafael <Anilmar-Rafael@gmail.com>
2023-08-01 09:41:22 -0300

    Primeiro commit

---
+++ src/main/java/br/com/etechoracio/viagem/Pw3ViagemApplication.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.viagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pw3ViagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pw3ViagemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/etechoracio/viagem/Pw3ViagemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.viagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pw3ViagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
