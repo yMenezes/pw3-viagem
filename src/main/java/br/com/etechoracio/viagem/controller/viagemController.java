00b8c01d040c384f37d6e013cc98836a8769c518
yMenezes <menezesgamer@hotmail.com>
2023-09-12 10:23:40 -0300

    GetById and Post

--- src/main/java/br/com/etechoracio/viagem/controller/viagemController.java
+++ src/main/java/br/com/etechoracio/viagem/controller/viagemController.java
@@ -3,11 +3,10 @@ package br.com.etechoracio.viagem.controller;
 import br.com.etechoracio.viagem.entity.Viagem;
 import br.com.etechoracio.viagem.repository.ViagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/viagens")
@@ -19,5 +18,17 @@ public class viagemController {
     @GetMapping
     public List<Viagem> buscarTodos() {
         return repository.findAll();
+    }
+
+    @GetMapping("/{Id}")
+    public Optional<Viagem> buscarPorId(@PathVariable Long Id)
+    {
+        return repository.findById(Id);
+    }
+
+    @PostMapping
+    public Viagem inserir(@RequestBody Viagem body) {
+        Viagem Inserido = repository.save(body);
+        return Inserido;
     }
 }
